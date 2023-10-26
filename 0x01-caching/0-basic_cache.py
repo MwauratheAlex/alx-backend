9adb9321d2081a47dbd2c55f6027210c3e9041d6
Mwaura Mbugua <mwaurathealex@gmail.com>
2023-10-26 11:55:48 +0300

    0-basic_cache.py

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""BasicCache module"""
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item in the cache"""
+        if (key is not None) and (item is not None):
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key"""
+        return self.cache_data.get(key, None)
