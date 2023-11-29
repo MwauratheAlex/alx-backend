79720cbd15e6b2cd7bc17bdfe7b7eb0ad806de45
Mwaura Mbugua <mwaurathealex@gmail.com>
2023-11-29 17:39:20 +0300

    add: 6-job_creator.js

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,23 @@
+import { createQueue } from "kue";
+
+const queue = createQueue({name: "push_notification_code"});
+
+const jobData = {
+    phoneNumber: "0713959070",
+    message: "Hello world"
+}
+
+const job = queue.create("push_notification_code", jobData);
+
+job
+    .on("enqueue", () => {
+        console.log(`Notification job created: ${job.id}`);
+    })
+    .on("complete", () => {
+        console.log(`Notification job completed`);
+    })
+    .on("failed", () => {
+        console.log(`Notification job failed`);
+    });
+
+job.save();
