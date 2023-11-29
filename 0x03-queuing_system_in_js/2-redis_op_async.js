6a6b70f5f8f47380b5a8a93bc174a8c80ecf8dca
Mwaura Mbugua <mwaurathealex@gmail.com>
2023-11-29 10:16:43 +0300

    add: 2-redis_op_async.js

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,28 @@
+import { createClient, print } from "redis";
+import { promisify } from 'util';
+
+const client = createClient();
+
+client.on('error', (err) => {
+    console.log(`Redis client not connected to the server: ${err}`);
+})
+
+client.on('connect', () => {
+    console.log("Redis client connected to the server")
+})
+
+const getAsync = promisify(client.get).bind(client);
+
+function setNewSchool(schoolName, value) {
+    client.set(schoolName, value, print);
+}
+
+async function displaySchoolValue(schoolName) {
+    const value = await getAsync(schoolName);
+    console.log(value);
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
+
