b9cc3b8a486031d68088c093c5b8cc2a46ee1457
Mbugua <mwaurathealex@gmail.com>
2024-02-09 16:46:28 +0300

    redis operations async

--- 0x03-queuing_system_in_js/2-redis_op_async.js
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -3,26 +3,25 @@ import { promisify } from 'util';
 
 const client = createClient();
 
-client.on('error', (err) => {
-    console.log(`Redis client not connected to the server: ${err}`);
-})
-
 client.on('connect', () => {
-    console.log("Redis client connected to the server")
+  console.log("Redis client connected to the server")
+});
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err}`);      
 })
 
 const getAsync = promisify(client.get).bind(client);
 
 function setNewSchool(schoolName, value) {
-    client.set(schoolName, value, print);
+  client.set(schoolName, value, print);
 }
 
 async function displaySchoolValue(schoolName) {
-    const value = await getAsync(schoolName);
-    console.log(value);
+  const value = await getAsync(schoolName);
+  console.log(value);
 }
 
 displaySchoolValue('Holberton');
 setNewSchool('HolbertonSanFrancisco', '100');
 displaySchoolValue('HolbertonSanFrancisco');
-
