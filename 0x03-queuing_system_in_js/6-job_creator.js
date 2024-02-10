ea4e8072a9d8045dad18a94a5b5f27d7e17c564c
Mbugua <mwaurathealex@gmail.com>
2024-02-10 10:45:34 +0300

    create a job creator and processor

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -1,23 +1,17 @@
 import { createQueue } from "kue";
 
-const queue = createQueue({name: "push_notification_code"});
+const queue = createQueue({ name: 'push_notification_code' });
 
 const jobData = {
-    phoneNumber: "0713959070",
-    message: "This is the code to verify your account"
-}
+  phoneNumber: '0713958070',
+  message: 'Hello from Mbugua'
+};
 
-const job = queue.create("push_notification_code", jobData);
+const job = queue.create('push_notification_code', jobData);
 
 job
-    .on("enqueue", () => {
-        console.log(`Notification job created: ${job.id}`);
-    })
-    .on("complete", () => {
-        console.log(`Notification job completed`);
-    })
-    .on("failed", () => {
-        console.log(`Notification job failed`);
-    });
+  .on('enqueue', () => console.log(`Notification job created: ${job.id}`))
+  .on('complete', () => console.log('Notification job completed'))
+  .on('failed', () => console.log('Notification job failed'))
 
 job.save();
