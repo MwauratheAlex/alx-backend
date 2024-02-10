87de8558167f797f6bae4f4eddce816bf6c7c1f7
Mbugua <mwaurathealex@gmail.com>
2024-02-10 09:37:29 +0300

    redis pubsub

--- 0x03-queuing_system_in_js/5-publisher.js
+++ 0x03-queuing_system_in_js/5-publisher.js
@@ -1,20 +1,18 @@
 import { createClient } from "redis";
 
-const client = createClient()
+const client = createClient();
 
-client.on("connect", () => {
-    console.log("Redis client connected to the server");
+client.on('connect', () => console.log('Redis client connected to the server'));
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err}`);
 });
 
-client.on("error", (err) => {
-    console.log(`Redis client not connected to the server: ${err}`);
-});
-
-const publishMessage = (message, time) => {
-    setTimeout(() => {
-        console.log(`About to send ${message}`);
-        client.publish("holberton school channel", message);
-    }, time);
+function publishMessage(message, time) {
+  setTimeout(() => {
+    console.log(`About to send ${message}`);
+    client.publish('holberton school channel', message);
+  }, time);
 }
 
 publishMessage("Holberton Student #1 starts course", 100);
